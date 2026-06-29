ab3d5b3ddb839a786f6cac3fe54771a02f5736c3
David <ldg.chelo@gmail.com>
2026-06-28 22:37:38 -0600

    Kiosco_pantalla_chica

--- kiosco.sh
+++ kiosco.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-xset s off 2>/dev/null
-xset s noblank 2>/dev/null
-xset -dpms 2>/dev/null
+# Forzar a que Chromium no busque sesiones anteriores rotas o alertas de cierre
+rm -rf ~/.config/chromium/Default/Preferences
+rm -rf ~/.config/chromium/Singleton*
 
-echo "🌐 Lanzando Chromium en modo Kiosco..."
-exec chromium --window-size=1920,1080 \
-              --window-position=0,0 \
-              --kiosk \
-              --noerrdialogs \
-              --disable-infobars \
-              --check-for-update-interval=31536000 \
-              --disable-pinch \
-              http://localhost:8000
+# 🚫 DESACTIVAR PROTECTOR DE PANTALLA Y AHORRO DE ENERGÍA INFINITO
+export DISPLAY=:0
+xset s off       # Desactiva el salvapantallas nativo
+xset _blank      # Evita que la pantalla se vaya a negro por inactividad
+xset -dpms       # Desactiva la administración de energía del monitor (DPMS)
+
+exec chromium \
+  --window-size=1280,800 \
+  --window-position=0,0 \
+  --kiosk \
+  --noerrdialogs \
+  --disable-infobars \
+  --check-for-update-interval=31536000 \
+  --disable-pinch \
+  --force-device-scale-factor=1.0 \
+  http://localhost:8000
